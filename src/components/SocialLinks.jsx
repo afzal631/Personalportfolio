8308f745800c99e7fe1a79e034263455c7a88990
mohammed afzal <afzalmd03@gmail.com>
2023-07-12 21:26:32 +0530

    third commit

--- src/components/SocialLinks.jsx
+++ src/components/SocialLinks.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import { FaGithub, FaLinkedin } from "react-icons/fa";
 import { HiOutlineMail } from "react-icons/hi";
 import { BsFillPersonLinesFill } from "react-icons/bs";
+import resume from "../assets/resume.pdf";
 
 export default function SocialLinks() {
   const links = [
@@ -35,16 +36,16 @@ export default function SocialLinks() {
       ),
       // style: "rounded-tr-md",
     },
-    {
-      id: 4,
-      visit: "/",
-      child: (
-        <>
-          Resume <BsFillPersonLinesFill size={30} />
-        </>
-      ),
-      style: "rounded-br-md",
-    },
+    // {
+    //   id: 4,
+    //   visit: { resume },
+    //   child: (
+    //     <>
+    //       Resume <BsFillPersonLinesFill size={30} />
+    //     </>
+    //   ),
+    //   style: "rounded-br-md",
+    // },
   ];
   return (
     <>
@@ -52,9 +53,11 @@ export default function SocialLinks() {
         <ul>
           {links.map(({ id, visit, child, style }) => {
             return (
-              <li key={id} className={`flex justify-between items-center w-40 h-14 px-4 bg-gray-500 ml-[-100px] hover:ml-[-10px] hover:rounded-md duration-500 ${style}`}>
+              <li
+                key={id}
+                className={`flex justify-between items-center w-40 h-14 px-4 bg-gray-500 ml-[-100px] hover:ml-[-10px] hover:rounded-md duration-500 ${style}`}
+              >
                 <a
-                  
                   href={visit}
                   target="#"
                   className="flex justify-between items-center w-full text-white"
@@ -64,6 +67,19 @@ export default function SocialLinks() {
               </li>
             );
           })}
+          <li
+            className="flex justify-between items-center w-40 h-14 px-4 bg-gray-500 ml-[-100px] hover:ml-[-10px] hover:rounded-md duration-500 rounded-br-md"
+          >
+            <a
+              download="resume.pdf"
+              href={resume}
+              className="flex justify-between items-center w-full text-white"
+            >
+              <>
+                Resume <BsFillPersonLinesFill size={30} />
+              </>
+            </a>
+          </li>
         </ul>
       </div>
     </>
